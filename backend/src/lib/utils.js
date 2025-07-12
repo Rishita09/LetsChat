59acaf7937834a0df0b3fad0fee24bea4e679add
Rishita09 <rishitashah2909@gmail.com>
2025-07-12 17:58:25 +0530

    Initial auth setup

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign(userId, process.env.JWT_SECRET, { expiresIn: "7d" });
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, //milliseconds
+    httpOnly: true, // prevents xss attacks cross-site scripting attacks basically not accessible via javascript
+    sameSite: "strict", //csrf attacks  cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
+  });
+
+  return token;
+};
